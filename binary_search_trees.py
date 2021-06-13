eeea02ad302b8badc5743b017461da8da54285ad
AlbertCerfeda <cerfeda.albert@gmail.com>
2021-06-13 16:44:43 +0200

    Added BST functions

--- binary_search_trees.py
+++ binary_search_trees.py
@@ -10,6 +10,45 @@ class Node:
         self.parent = None
 ##############
 
+def right_rotate (t):
+    assert t != None and t.left != None
+    r = t.left
+    t.left = r.right
+    r.right = t
+    return r
+
+def left_rotate (t):
+    assert t != None and t.right != None
+    r = t.right
+    t.right = r.left
+    r.left = t
+    return r
+
+
+t = Node(5)
+t.left = Node(3)
+t.left.left = Node(2)
+t.left.right = Node(4)
+t.right = Node(7)
+t.right.right = Node(8)
+t.right.right.right = Node(9)
+
+def bst_min_node(t):
+    if t == None:
+        return None
+    while t.left != None:
+        t = t.left
+    return t
+
+def bst_max_node(t):
+    if t == None:
+        return None
+    while t.right != None:
+        t = t.right
+    return t
+
+
+
 def bst_next(t):
     # next node in the natural order of keys
     if t.right != None:
@@ -35,6 +74,17 @@ def bst_previous(t):
         t = p
         p = p.parent
     return p
+
+
+def bst_straighten(t):
+    while t.left != None:
+        t = right_rotate(t)
+    n = t
+    while n.right != None:
+        n = n.right
+        while n.left != None:
+            n = right_rotate(n)
+    return t
 
 
 def bst_search(t, k):
@@ -66,3 +116,7 @@ def bst_insert(t,k):
                 return r
             else:
                 t = t.right
+
+#def bst_delete(t,k):
+
+
